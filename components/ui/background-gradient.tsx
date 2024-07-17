bcaef28de2df34e50d1e0778430d90471a1baa93
Luis Felipe Gaitan Zambrano <87459956+luisfegz@users.noreply.github.com>
2024-07-16 23:21:23 -0500

    Update background-gradient.tsx

--- components/ui/background-gradient.tsx
+++ components/ui/background-gradient.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils"
 
 export const BackgroundGradient = ({
   children,
