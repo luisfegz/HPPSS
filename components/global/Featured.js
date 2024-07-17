715d64cfc5610b07fc981ad9b40b9d1051ea76e2
Luis Felipe <luisfelipegaitanzambrano@gmail.com>
2024-07-16 23:05:40 -0500

    error

---
+++ components/global/Featured.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Center from './Center';
+import Wrapper from './Featured/Wrapper';
+import CartIcon from '../icons/CartIcon';
+
+function Featured({product}) {
+  return (
+    <div className='bg-black'>
+      <Center>
+        <Wrapper>
+          <div className='flex ml-4 items-center'>
+            <div>
+              <h1 className='font-bold md:font-black mb-2 text-lg md:text-xl lg:text-2xl text-white'>{product.title}</h1>
+              <p className='text-[.6rem] text-white md:text-[1.1rem] lg:text-[1.3rem]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, repudiandae?</p>
+              {/* <------------------------------------------------------------ Button Read More REMEMBER ---------------------------------------------------------------------------------------------------------------------> */}
+              <div className='flex mt-3.5 md:mt-4 lg:mt-5'>
+                <div>
+                  <button className="inline-flex h-8 md:h-10 lg:h-14 text-sm md:text-lg lg:text-2xl animate-shimmer items-center justify-center z-40
+                      rounded-md border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+                      bg-[length:200%_100%] px-2 md:px-2 lg:px-3 font-light md:font-normal lg:font-medium text-slate-100 transition-colors focus:outline-none 
+                      focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  >
+                    Leer+
+                  </button>
+                </div>
+                {/* <------------------------------------------------------------ Button with functions of add to cart REMEMBER -----------------------------------------------------------------------------------------------> */}
+                <div className='ml-8'>
+                  <button className="inline-flex h-8 md:h-10 lg:h-14 text-sm md:text-lg lg:text-2xl animate-shimmer items-center justify-center z-40
+                      rounded-md border border-zinc-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+                      bg-[length:200%_100%] px-2 md:px-2 lg:px-3 font-light md:font-normal lg:font-medium text-slate-100 transition-colors focus:outline-none 
+                      focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                  >
+                    Añadir <CartIcon /> 
+                  </button>
+                </div>
+                {/* <-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------> */}
+              </div>
+            </div>
+          </div>
+          <div>
+            <img className='max-w-[100%]' src="https://luisfelipegaitan-next-ecommerce.s3.amazonaws.com/1695618706972.png" alt="Image" />
+          </div>
+        </Wrapper>
+      </Center>
+    </div>
+  );
+}
+
+export default Featured;
